b92406d46791c2a394a5dbd068452d8e261b58d6
GuyKroizman <guykr@wix.com>
2023-02-03 20:13:28 +0200

    what do you care

--- RootsTest/Assets/GameManager.cs
+++ RootsTest/Assets/GameManager.cs
@@ -8,32 +8,40 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Camera _camera;
     [SerializeField] private GameObject seedPrefab;
     [SerializeField] private float seedInstantiationHeightOffset;
+    [SerializeField] private float shootStrength    ;
 
-
-    private void Start()
-    {
-        Enemy[] en = GetComponents<Enemy>();
-        foreach (var e in en)
-        {
-            e.SetAgentTarget(transform);
-        }
-    }
 
     private void Update()
     {
-
-        if (Input.GetMouseButtonDown(0))
+        
+        if (Input.GetMouseButton(0))
         {
-            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, 100))
-            {
-                Debug.Log(hit.transform.name);
-                Debug.Log("hit");
-                var pos = new Vector3(hit.point.x, hit.point.y + seedInstantiationHeightOffset, hit.point.z);
-                Instantiate(seedPrefab, pos, Quaternion.identity);
-            }
+            shootStrength += Time.deltaTime;
         }
+        if(Input.GetMouseButtonUp(0))
+        {
+            if (shootStrength > 0.1f)
+            {
+                Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, 100))
+                {
+                    var pos = new Vector3(hit.point.x, hit.point.y + seedInstantiationHeightOffset, hit.point.z);
+
+                    Seed seed = Instantiate(seedPrefab, pos, Quaternion.identity).GetComponent<Seed>();
+                    seed.shoot(shootStrength);
+
+                    shootStrength = 0;
+                }
+            }
+
+           
+        }
+        
+          
+            
+		}
     
-    }
+    
 }
